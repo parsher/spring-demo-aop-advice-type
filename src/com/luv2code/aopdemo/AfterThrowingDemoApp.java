0266f6e66e74139a03a5719ca2dab2ed502100a7
parsher <26663229+parsher@users.noreply.github.com>
2020-02-19 15:56:16 +0900

    @AfterThrowing

--- src/com/luv2code/aopdemo/AfterThrowingDemoApp.java
+++ src/com/luv2code/aopdemo/AfterThrowingDemoApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.luv2code.aopdemo.dao.AccountDAO;
 
-public class AfterReturningDemoApp {
+public class AfterThrowingDemoApp {
 
 	public static void main(String[] args) {
 		// read spring config java class
@@ -14,14 +14,21 @@ public class AfterReturningDemoApp {
 
 		// get the bean from spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
-		
+
 		// call method to find the accounts
-		List<Account> theAccounts = theAccountDAO.findAccounts(false);
-		
-		System.out.println("\n\nMain Program: AfterReturningDemoApp");
+		List<Account> theAccounts = null;
+
+		try {
+			boolean tripWire = true;
+			theAccounts = theAccountDAO.findAccounts(tripWire);
+		} catch (Exception ex) {
+			System.out.println("\n\nMain program catch the Error\n" + ex.toString());
+		}
+
+		System.out.println("\n\nMain Program: AfterThrowingDemoApp");
 		System.out.println(theAccounts);
 		System.out.println("\n");
-		
+
 		// close the context
 		context.close();
 	}
--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -16,6 +17,18 @@ import com.luv2code.aopdemo.Account;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+	
+	@AfterThrowing(pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))", throwing="theExc")
+	public void afterThrowingFindAccountsAdvice(JoinPoint theJoinPoint, Throwable theExc) {
+		
+		// print out which method we get.
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n===> Excuting @AfterThrowing on method: " + method);
+		// log the exception
+		
+		System.out.println("\n===> The exception is: " + theExc);
+	}
+	
 	
 	// add a new advice for @AfterReturning on the findAccounts method
 	@AfterReturning(pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))", returning="result")
